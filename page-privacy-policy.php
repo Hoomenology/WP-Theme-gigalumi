0374722a53d823b80ffd162776b42a4b8b33d78e
xoinstein <xoinstein@gmail.com>
2020-12-25 13:35:52 +0800

    add static-page

---
+++ page-privacy-policy.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package storefront
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area static-page">
+        <div class="container ">
+            <h1>Privacy Policy</h1>
+            <p>We care about our customers and know that your privacy is important to you. This Privacy Policy (&quot;Privacy Policy&quot;) is our way of sharing how we collect your information, why we need it, and how we use it.</p>
+            <h4>Scope of Application</h4>
+            <p>This Privacy Policy applies to your use of our websites and mobile websites (together, the &quot;Site&quot;, and together with the Site, the &quot;Service&quot;). The Service includes websites and mobile applications operated by Global Amiba Network Technology Inc., including gigalumi.com. If you visit another website, including another Gigalumi website, the privacy policy of that website will apply. Please review the privacy policy of that website to understand how that website operator processes your information.</p>
+            <p>This Privacy Policy also applies to the information you provide to us after being presented with or directed to this Privacy Policy. This could be, for example, when you share your information with our service representatives (e.g. a customer service agent, design professional, or an installation or assembly pro) or send us a letter, email or chat.</p>
+            <p>Before submitting information to the Service, please review this Privacy Policy carefully.</p>
+            <p>The purpose of this Privacy Policy is to inform you of:</p>
+            <ul>
+            <li>the personal information we may collect from you when you visit our web site, respond to our emails, or otherwise contact us via telephone, email,</li>
+            <li>why we gather information from you,</li>
+            <li>how we collect it,</li>
+            <li>how we use it, and</li>
+            <li>the choices you have regarding our use of personal information you have provided.</li>
+
+            </ul>
+            <p></p>
+            <p>This Privacy Policy is part of the <a href="/terms-conditions/" target="_blank"><strong>Terms of Use</strong></a>, which governs your use of our web site located at <a href="https://www.gigalumi.com/" target="_blank"><strong><a href='http://www.gigalumi.com' target='_blank' class='url'>www.gigalumi.com</a></strong></a>.</p>
+            <p> <strong>Note:</strong> <em>You must</em> be 18 years or older to use, subscribe or register as a member of gigalumi.com. The Web Site is a general audience site and we do not knowingly collect, maintain or disclose personal information from or about children.</p>
+            <h4>Why do we gather information?</h4>
+            <p>Gigalumi gathers personal information to help improve our products and customer service, to communicate with you, to process your orders, to provide an enhanced and more personalized shopping experience and to inform you, and your gift and message recipients, of special offers and discounts from us, our affiliated brands and selected third parties.</p>
+            <h4>Where do we gather information from?</h4>
+            <ul>
+            <li><p>You:</p>
+            <ul>
+            <li>when you provide such information directly to us</li>
+            <li>when Personal Data about you is automatically collected in connection with your use of our Services</li>
+
+            </ul>
+            </li>
+            <p></p>
+            <li><p>Our subsidiaries and affiliates (together, “Affiliates”), when they provide us with Personal Data about you</p>
+            </li>
+            <li><p>Third parties, when they provide us with Personal Data about you (“Third Parties”). Third Parties that share your Personal Data with us include:</p>
+            <ul>
+            <li>Service providers. For example, we may use analytics service providers to analyze how you interact and engage with the Services, or Third Parties may help us provide you with customer support.</li>
+            <li>Social networks connected to the Services. If you provide your social network account credentials to us or otherwise sign in to the Services through a third-party site or service, some content and/or information in those accounts may be transmitted into your Account with us.</li>
+            <li>Advertising partners. We receive information about you from some of our service providers who assist us with marketing or promotional services related to how you interact with our websites, applications, products, services, advertisements or communications.</li>
+
+            </ul>
+            </li>
+
+            </ul>
+            <h4>What information do we gather?</h4>
+            <p>We collect information through our Web Site, emails, mail, retail store transactions, fax, and telephone, including when you:</p>
+            <ul>
+            <li>place an order,</li>
+            <li>participate in our forums, surveys, contests, sweepstakes, promotions, content submissions, chats, bulletin boards, discussion groups, requests for suggestions, and membership registrations, and</li>
+            <li>engage in other activities, services, products and resources we make accessible to our customers, members or users.</li>
+
+            </ul>
+            <p></p>
+            <p>Depending on how you interact with us, we collect some or all of the following types of information:</p>
+            <ul>
+            <li>your name, address, telephone number, email address,</li>
+            <li>billing information (credit card number, expiration date and billing address),</li>
+            <li>gender and birth date, if you choose to enter such information,</li>
+            <li>products purchased and occasion type (for example, Mother&#39;s Day),</li>
+            <li>Message and gift recipients&#39; names, addresses, telephone numbers and email addresses, and</li>
+            <li>other information you provide to us, including User Submissions (defined in the <a href="/terms-conditions/" target="_blank"><strong>Terms of Use</strong></a>).</li>
+
+            </ul>
+            <p></p>
+            <p>Information you provide to us may be collected by us even if an order, registration, or other process is started but not completed or otherwise canceled. Members may also add to their Address Book the name, address and telephone number of friends or other persons to whom they may wish to send gifts or expressions. Only Members have the right to view and edit their Address Book entries.</p>
+            <h4>Information Collected Automatically</h4>
+            <p>The Services use cookies and similar technologies such as pixel tags, web beacons, clear GIFs and JavaScript (collectively, “Cookies”) to enable our servers to recognize your web browser and tell us how and when you visit and use our Services, to analyze trends, learn about our user base and operate and improve our Services. Cookies are small pieces of data – usually text files – placed on your computer, tablet, phone or similar device when you use that device to use our Services. We may also supplement the information that we collect from you with information received from Third Parties, including Third Parties that have placed their own Cookies on your device(s). Please note that because of our use of Cookies, the Services do not support “Do Not Track” requests sent from a browser at this time.</p>
+            <p>We use the following types of Cookies:</p>
+            <ul>
+            <li>l Essential Cookies. Essential Cookies are required for providing you with features or services that you have requested. For example, certain Cookies enable you to log into secure areas of our Services. Disabling these Cookies may make certain features and services unavailable.</li>
+            <li>l Functional Cookies. Functional Cookies are used to record your choices and settings regarding our Services, maintain your preferences over time and recognize you when you return to our Services. These Cookies help us to personalize our content for you, greet you by name and remember your preferences (for example, your choice of language or region).</li>
+            <li>l Performance/Analytical Cookies. Performance/Analytical Cookies allow us to understand how visitors use our Services, such as by collecting information about the number of visitors to the Services, what pages visitors view on our Services, what actions a visitor performs on our Services and how long visitors are viewing pages on the Services. Performance/Analytical Cookies also help us measure the performance of our advertising campaigns in order to help us improve our campaigns and the Services’ content for those who engage with our advertising. For example, Google, Inc. (“Google”) uses cookies in connection with its Google Analytics services. Google’s ability to use and share information collected by Google Analytics about your visits to the Services is subject to the <a href="https://policies.google.com/terms?hl=en" target="_blank"><strong>Google Analytics Terms of Use</strong></a>  and the <a href="https://policies.google.com/privacy?hl=en" target="_blank"><strong>Google Privacy Policy</strong></a>. You have the option to opt out of Google’s use of Cookies by visiting the Google advertising opt-out page at or the Google Analytics Opt-out Browser Add-on.</li>
+            <li>Re-targeting/Advertising Cookies. Re-targeting/Advertising Cookies collect data about your online activity and identify your interests so that we can provide advertising that we believe is relevant to you. For more information about this, please see the section below titled “Information about Interest-Based Advertisements.”</li>
+
+            </ul>
+            <p></p>
+            <p>You can decide whether or not to accept Cookies through your internet browser’s settings. Most browsers have an option for turning off the Cookie feature, which will prevent your browser from accepting new Cookies, as well as (depending on the sophistication of your browser software) allow you to decide on acceptance of each new Cookie in a variety of ways. </p>
+            <p>You can also delete all Cookies that are already on your device. If you do this, however, you may have to manually adjust some preferences every time you visit our website and some of the Services and functionalities may not work.</p>
+            <h4>How we use &quot;Cookies&quot; and &quot;Action Tags&quot;?</h4>
+            <p>A &quot;cookie&quot; is a small data file sent to your browser from a web server and stored on your computer. We use cookies to let us know that you are a prior customer, user or Member and to provide certain features to you. Most web browsers allow you to instruct the browser to prevent the use of cookies. However, if you disable this feature, certain aspects of the Gigalumi Web Site, such as the shopping basket, may not function properly.</p>
+            <p>We have relationships with third parties who may receive personal information directly from the cookies placed by our Web Site on your computer to assist in analyzing our business and customers.</p>
+            <p>Our Web Site also uses a common web technology called &quot;action tags&quot; (also called single pixel gifs or web beacons) to anonymously track purchase, usage and other web activity information. Action tags (including third party tags) may be used to provide data to third parties for tracking purposes. However, our Web Site does not use action tags to collect or transfer personal information.</p>
+            <h4>How we use the information we collect?</h4>
+            <p>We use information we collect to communicate with you, to process your orders, to provide an enhanced and more personalized shopping experience, to inform you and your gift and message recipients of offers and discounts from Gigalumi and our affiliated brands and to enable you to receive credits for purchases under loyalty or buying programs which you have joined. In addition, we may make such information (except for credit card number and expiration date, unless you consent otherwise), including aggregated information, available to selected third parties including, but not limited to, those who share or rent information for direct marketing purposes.</p>
+            <p>However, without your consent, we do not make your, or your gift or message recipients&#39; email addresses, available to third parties. In addition, you may instruct that other personal information about you or your message or gift recipients&#39; that you have provided to us not be shared with third parties by following the instructions in <em>How to Limit the Use of Personal Information</em>, below.</p>
+            <p>Our Affiliates, subcontractors, and agents, and any successors to our business (if we ever sell some or all of our business), are <em>not</em> considered to be third parties under this Policy. However, in connection with information collected under this Policy, they are required to comply with this Policy. &quot;Affiliates&quot; are persons or entities directly or indirectly controlling, controlled by, or under common control with Gigalumi.</p>
+            <p>We may combine information we receive from you via our Web Site, emails, mail, fax, and telephone with information from our Affiliates and third parties. We use the combined information to enhance and personalize your shopping experience with us and to communicate with you in accordance with this Privacy Policy.</p>
+            <p>We reserve the right to disclose information you provide to us as required by law, in response to legal process and law enforcement requests and as necessary or appropriate, in our view, to operate the Web Site, process orders or registrations, to conduct promotions, contests, and sweepstakes you participate in, and to protect the rights or property of the Company, its Affiliates, users, customers, recipients, Members, Sponsors, Providers, Licensors, Merchants and Associates (as these terms are defined in the <a href="/terms-conditions/" target="_blank"><strong>Terms of Use</strong></a>).</p>
+            <p><strong><em>Please note that any personal information you reveal or post on one our public forums (as defined in the <a href="/terms-conditions/" target="_blank">Terms of Use),</a> such as a message board, chat room, discussion group, folder, survey, contest, sweepstakes, user review and rate forum, and all User Submissions, are not protected by this Privacy Policy and may be collected, shared and used by us or with third parties, including to contact you. Information you give out or post on our public forums you disclose at your own risk.</em></strong></p>
+            <h4><strong>Information about Interest-Based Advertisements</strong></h4>
+            <p>We serve advertisements, and also allow third-party ad networks, including third-party ad servers, ad agencies, ad technology vendors and research firms, to serve advertisements through the Services. These advertisements may be targeted to users who fit certain general profile categories or display certain preferences or behaviors (“Interest-Based Ads”). Information for Interest-Based Ads (including Personal Data) may be provided to us by you, or derived from the usage patterns of particular users on the Services and/or services of Third Parties. Such information may be gathered through tracking users’ activities across time and unaffiliated properties, including when you leave the Services. To accomplish this, we or our service providers may deliver Cookies, including a file (known as a “web beacon”) from an ad network to you through the Services. Web beacons allow ad networks to provide anonymized, aggregated auditing, research and reporting for us and for advertisers. Web beacons also enable ad networks to serve targeted advertisements to you when you visit other websites. Web beacons allow ad networks to view, edit or set their own Cookies on your browser, just as if you had requested a web page from their site.</p>
+            <p>We comply with the Digital Advertising Alliance (“DAA”) Self-Regulatory Principles for Online Behavioral Advertising. Through the DAA and Network Advertising Initiative (“NAI”), several media and marketing associations have developed an industry self-regulatory program to give consumers a better understanding of, and greater control over, ads that are customized based a consumer’s online behavior across different websites and properties. To make choices about Interest-Based Ads from participating third parties, including to opt out of receiving behaviorally targeted advertisements from participating organizations, please visit the DAA’s or NAI’s consumer opt-out pages.</p>
+            <h4>Consent to Transfer and Submission</h4>
+            <p>Gigalumi is operated in the United States. If you are located outside the United States, please be aware that any information you provide to us will be transferred to the United States. By using the Web Site or otherwise providing us with information, you hereby consent to this transfer.</p>
+            <p>In addition, if you submit data to us about other individuals (such as your message and gift recipients), you agree that you have obtained consent from each such individual to the submission, transfer and processing of the information. Although personal information collected by us through the Web Site, emails, mail, fax, and telephone is used by us in accordance with this Privacy Policy, laws generally applicable to the protection of personal data in the U.S. may not be as stringent as those in other countries.</p>
+            <h4>Links to Other Web Sites</h4>
+            <p>Gigalumi wants you to be aware that when you click on links and/or ad banners that take you to affiliated or third-party web sites, you will be subject to the privacy policies and <a href="/terms-conditions/" target="_blank"><strong>Terms of Use</strong></a> of those web sites, not ours. We encourage you to read the posted privacy statement and user terms whenever using, and prior to providing any personal information to, any other web site.</p>
+            <h4>Security and Passwords</h4>
+            <p>If you place an order on our Web site, it encrypts the credit card number you submit prior to transmission over the Internet using secure socket layer (SSL) encryption technology. This technology works best when the Web Site is viewed using Microsoft&#39;s Internet Explorer® or Netscape Navigator®. However, no transmission of data over the Internet or any other network can be guaranteed to be 100% secure. Although we make reasonable efforts to safeguard personal information once we receive it, we cannot warrant the security of information we receive.</p>
+            <p>Portions of the Web Site (such as our Member registration and Address Book) may require registration and log-in processes in which you will select a user ID and password (collectively, the &quot;Password&quot;). Passwords provided to you by us are the confidential property of the Company and may be used by you solely for your individual use of the Web Site (and otherwise as specified by us). You are responsible for maintaining the confidentiality of any Password and for all activities that occur using your Password, whether or not authorized by you. You agree to immediately notify us of any unauthorized use of your Password or accounts.</p>
+        </div>
+        
+	</div><!-- #primary -->
+
+<?php
+do_action( 'storefront_sidebar' );
+get_footer();
---
+++ page-return-exchange-policy.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package storefront
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area static-page">
+        <div class="container ">
+            <h1>Returns &amp; Exchange Policy</h1>
+            <p>We want you to be happy with your order. If something isn’t right, let us know.</p>
+            <p>If, for any reason, you want return it, you need for a refund within 30 days. Only items purchased from <a href='http://www.gigalumi.com' target='_blank' class='url'>www.gigalumi.com</a> with original receipt will be considered for refund or exchange. If you purchased a product from an authorized Gigalumi retailer, please contact that retailer for assistance with your refund.</p>
+            <h4>What is your return policy?</h4>
+            <p>We do not accept returns for products which are not broken or delivered incorrectly. After receiving your product, you have 30 days to return exchange it. To qualify for a refund, products need to be complete with all components and original packaging. Please note that we currently do not offer gift exchanges.</p>
+            <p>For returns, we’ll gladly refund your purchase price, excluding shipping and handling charges. There are no restocking fees. (Please see conditions below.)</p>
+            <p>When you receive your order, our contact information will be displayed in your order confirmation email. Please keep any emails you receive regarding your order, as they may contain important information.</p>
+            <p>Once we receive your return, we will process a refund within 14 business days to the original form of payment. Your bank may require additional time to post this transaction to your account.</p>
+            <p>For help processing a return, please give our friendly team a email at <a href='mailto:service@gigalumi.com' target='_blank' class='url'>service@gigalumi.com</a>.</p>
+            <h4>When will I receive a refund?</h4>
+            <p>Just as fast as we can get it to you! Any credits on your account with us will be refunded promptly as soon as we have received and processed the return. Once we receive your return, we will process a refund within 14 business days to the original form of payment. Your bank may require additional time to post this transaction to your account. We’ll use the same method to refund the amount as you used when you placed the order. For example, if the order was paid by credit card, we will credit your credit card account.</p>
+            <h4>Replacement Parts &amp; Missing Parts</h4>
+            <p>If you’re missing a piece, or received a damaged item, please give us a call at 1-888-829-9252 or email us at <a href='mailto:service@gigalumi.com' target='_blank' class='url'>service@gigalumi.com</a> and we’ll do our very best to help you figure out your options. </p>
+            <p>We’ll need some product info so please have either the product in front of you or a photo of the product label you can refer to, if that’s more convenient.</p>
+            <h4>Damaged Products</h4>
+            <p>Accidents happen, unfortunately. If a shipment arrives at your door with apparent shipping damage, please refuse the delivery. If you have already accepted delivery and then find shipping damage, please contact us immediately. It will be helpful if you send us the picture of the damaged package or products.</p>
+        </div>
+	</div><!-- #primary -->
+
+<?php
+do_action( 'storefront_sidebar' );
+get_footer();
