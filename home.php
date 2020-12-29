f8737d6306e76b72774225e949a7e9b056bc5d3d
xoinstein <xoinstein@gmail.com>
2020-12-29 17:02:28 +0800

    update home-banner

--- home.php
+++ home.php
@@ -63,25 +63,31 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 
         <div class="home-banner">
-                <div class="container banner-inner h-full">
-                    <div class="banner-content flex flex-col h-full justify-center items-left">
-                        <h1 class="text-white font-bold">WE ILLUMINATE THE NIGHT WITH SUNLIGHT</h1>
-                        <a class="go-shop inline-block border border-white rounded-3 w-40 text-white text-center my-3 p-3" href="">SHOP HALLOWEEN</a>
-                    </div>
+            <div class="container banner-inner h-full relative">
+                <div class="banner-off absolute -top-1 -left-1 text-white font-bold  text-lg flex flex-col items-center pt-2 px-9 pb-15 "><p>UP TO</p><p class=" text-5xl leading-10">50%</p><p>OFF</p>
                 </div>
+                <img class="absolute bottom-0 right-0" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/home-banner-product.webp" alt="">
+                <div class="banner-content flex flex-col h-full justify-center items-left text-white">
+                    <p class="text-5xl font-bold pt-28">WE NOW LAUNCHEND!</p>
+                    <p class="pb-4 text-3xl leading-10">Our new website is online now, <br/>Come and celebrate with us!</p>
+                    <p class="pb-4 text-xl">+ Free delivery over $59</p>
+                    <p class="pb-4 text-blue-300">Special Offer Duration: 8 Jan 2021 to 31 Jan 2021</p>
+                    <a class="go-shop inline-block border-2 border-white rounded-5 w-40 text-center font-semibold my-3 p-3" href="/shop">SHOP ALL SALES</a>
+                </div>
+            </div>
         </div>
 
         <div class="shop-info container flex justify-around items-center h-24 text-base text-black ">
             <div class="shop-info-item flex items-center">
-                <svg class="w-10 h-10 mx-5 text-blue-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 22 22" stroke="currentColor"><path fill="#fff" d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0" /></svg>
+                <svg class="w-10 h-10 mx-5 text-blue-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 22 22" stroke="currentColor"><path fill="#fff" d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0" /></svg>
             <p>Free Shipping on Orders Over $99</p>
             </div>
             <div class="shop-info-item flex items-center">
-                <svg class="w-10 h-10 mx-5 text-blue-300" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M864 483.2V384a352 352 0 0 0-704 0v99.2A160 160 0 0 0 192 800h96a64 64 0 0 0 64-64v-192a64 64 0 0 0-64-64H224v-96a288 288 0 0 1 576 0v96h-64a64 64 0 0 0-64 64v192a64 64 0 0 0 64 64h58.24A96 96 0 0 1 704 864h-101.76a96 96 0 1 0 0 64H704a160 160 0 0 0 157.12-130.88A160 160 0 0 0 864 483.2zM160 730.56a96 96 0 0 1 0-181.12zM288 544v192H224v-192z m224 384a32 32 0 1 1 32-32 32 32 0 0 1-32 32z m288-192h-64v-192h64z m64-5.44v-181.12a96 96 0 0 1 0 181.12z" fill="currentColor"></path></svg>
+                <svg class="w-10 h-10 mx-5 text-blue-400" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M864 483.2V384a352 352 0 0 0-704 0v99.2A160 160 0 0 0 192 800h96a64 64 0 0 0 64-64v-192a64 64 0 0 0-64-64H224v-96a288 288 0 0 1 576 0v96h-64a64 64 0 0 0-64 64v192a64 64 0 0 0 64 64h58.24A96 96 0 0 1 704 864h-101.76a96 96 0 1 0 0 64H704a160 160 0 0 0 157.12-130.88A160 160 0 0 0 864 483.2zM160 730.56a96 96 0 0 1 0-181.12zM288 544v192H224v-192z m224 384a32 32 0 1 1 32-32 32 32 0 0 1-32 32z m288-192h-64v-192h64z m64-5.44v-181.12a96 96 0 0 1 0 181.12z" fill="currentColor"></path></svg>
                 <p>Reply Within 24 hours</p>
             </div>
             <div class="shop-info-item flex items-center">
-                <svg class="w-10 h-10 mx-5 text-blue-300" viewBox="0 0 26 26" role="presentation">
+                <svg class="w-10 h-10 mx-5 text-blue-400" viewBox="0 0 26 26" role="presentation">
                     <g transform="translate(1 1)" stroke-width="1.5" fill="none" fill-rule="evenodd">
                         <path stroke="currentColor" d="M8 4h8v7"></path>
                         <path stroke="currentColor" stroke-linecap="square" d="M11 7L8 4l3-3"></path>
