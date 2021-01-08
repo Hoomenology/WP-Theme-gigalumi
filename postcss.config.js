4469bced84f7a8e838253a5c050e5a316b3a0ea8
xoinstein <xoinstein@gmail.com>
2021-01-08 09:54:26 +0800

    update open prugecss

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,13 @@ module.exports = {
   plugins: [
     require("tailwindcss"),
     require("autoprefixer"),
-    // require("@fullhuman/postcss-purgecss")({
-    //   content: [
-    //     './assets/css/*.css',
-    //     './inc/template-function.php',
-    //   ],
-    //   defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-    // }),
+    require("@fullhuman/postcss-purgecss")({
+      content: [
+        './assets/css/*.css',
+        './inc/template-functions.php',
+        './*.php',
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
   ],
 };
