a78b9f54c4952dfe8d4a1dccc50059c1e0aa687a
xoinstein <xoinstein@gmail.com>
2021-01-13 09:41:06 +0800

    add subscribe

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -470,10 +470,38 @@ function gigalumi_footer_container() {
                 </div>
                 <div class="footer-contact flex flex-col text-base">
                     <div class="subscribe my-3">
+                        <!-- Begin Mailchimp Signup Form -->
+                        <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+                        <style type="text/css">
+                            #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+                            /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+                            We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+                        </style>
+                        <div id="mc_embed_signup">
+                            <form action="https://gigalumi.us4.list-manage.com/subscribe/post?u=f41749479b267496272fda01c&amp;id=a466065527" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                                <div id="mc_embed_signup_scroll">
+                                    <p class="font-semibold">BE THE FIRST TO KNOW ABOUT OUR SALES!</p> 
+                                    <div class="mc-field-group">
+                                        <label for="mce-EMAIL"></label>
+                                        <input type="email" placeholder="Email Address" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                                    </div>
+                                    <div id="mce-responses" class="clear">
+                                        <div class="response" id="mce-error-response" style="display:none"></div>
+                                        <div class="response" id="mce-success-response" style="display:none"></div>
+                                    </div>
+                                    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_f41749479b267496272fda01c_a466065527" tabindex="-1" value=""></div>
+                                    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                                </div>
+                            </form>
+                        </div>
+                        <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email'; }(jQuery));var $mcj = jQuery.noConflict(true);</script>
+                        <!--End mc_embed_signup-->
+<!--                         
                         <form action="">
-                            <p class="font-semibold">BE THE FIRST TO KNOW ABOUT OUR SALES!</p>
+                            
                             <label class="my-6 flex h-14 text-center border border-gray-800 rounded-5" for="email"><input class="flex-1 mx-3 bg-gray-100" type="email" name="email" id="email" placeholder="Email Address"><input class="bg-black text-white font-bold h-full px-2" type="submit" value="SUBSCRIBE"></label>
-                        </form>
+                        </form> -->
                     </div>
                     <div class="call-us">
                         <p class="font-semibold">QUESTIONS? FEEDBACK? CALL US AT</p>
