5f3dac420d139a2ba5202ce6576ac1fba854b775
xoinstein <xoinstein@gmail.com>
2021-01-21 17:46:30 +0800

    update help link

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -20,7 +20,7 @@ function gigalumi_header_container() {
         </div>
         <div class="header-action md:order-last flex-1 md:divide-x flex flex-row items-center text-xs">
             <div class="help hidden md:block mx-7">
-                <a href="/help" class="flex flex-row items-center mx-3">
+                <a href="/customer-service/" class="flex flex-row items-center mx-3">
                     <svg class="h-6 w-6 mx-3 text-blue-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
                     </svg>
